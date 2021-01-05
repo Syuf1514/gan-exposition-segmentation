4978730d56299421cb3fd240524b547d7f21f4eb
Syuf1514 <synaps1996@gmail.com>
2021-01-05 19:19:08 +0300

    Updated arguments parsing.

--- scripts/download_weights.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir ../weights
-cd ../weights
-
-# BigBiGAN-pytorch weights
-wget https://www.dropbox.com/s/9w2i45h455k3b4p/BigBiGAN_x1.pth
-# background darkening and foreground lightening direction
-wget https://www.dropbox.com/s/np74kmkohkbx76t/bg_direction.pth
---
+++ segmlib/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+root_path = Path(__file__).resolve().parents[1]
+weights_path = root_path / 'weights'
+embeddings_path = root_path / 'embeddings'
