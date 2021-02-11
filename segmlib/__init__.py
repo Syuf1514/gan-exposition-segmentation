e94bd34f64610a8cc6fdc211e761b836121de6d7
Syuf1514 <synaps1996@gmail.com>
2021-02-11 15:15:30 +0300

    Removed affine mask generator.

--- segmlib/__init__.py
+++ segmlib/__init__.py
@@ -2,5 +2,5 @@ from .biggan import UnconditionalBigGAN
 from .unet import UNet
 from .datasets import GenerationDataset, SegmentationDataset
 from .model import SegmentationModel
-from .mask_generators import AffineMaskGenerator, EMMaskGenerator
+from .mask_generators import EMMaskGenerator
 from .metrics import accuracy, binary_iou, binary_fbeta
